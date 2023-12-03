b5e50a25ad2c6fdf348eecc5860caa25434181b8
Tim Garvin <tcgarvin@gmail.com>
2023-12-03 10:16:12 -0500

    Finish Day 3

--- 03/03.py
+++ 03/03.py
@@ -33,9 +33,12 @@ def get_adjacent_coords(coord):
     ]
 
 
+NO_GEAR = (-10,-10)
+
 def get_number_value(grid, coord):
     number_finished = False
     adjacent_symbol = False
+    gear_coord = NO_GEAR
     number = ""
     cursor = coord
     while not number_finished:
@@ -43,6 +46,9 @@ def get_number_value(grid, coord):
         for adjacent_coord in get_adjacent_coords(cursor):
             if is_symbol(grid[adjacent_coord]):
                 adjacent_symbol = True
+            if grid[adjacent_coord] == "*":
+                gear_coord = adjacent_coord
+
 
         cursor = (cursor[0]+1, cursor[1])
         next_character = grid[cursor]
@@ -51,9 +57,9 @@ def get_number_value(grid, coord):
 
     #print(coord, number, adjacent_symbol)
     if not adjacent_symbol:
-        return 0
+        return 0, NO_GEAR
 
-    return int(number)
+    return int(number), gear_coord
 
 
 def solve_part_1(grid, dimensions):
@@ -63,12 +69,31 @@ def solve_part_1(grid, dimensions):
         for i in range(max_i+1):
             char = grid[(i,j)]
             if is_digit(char) and (not is_digit(grid[(i-1,j)])):
-                total += get_number_value(grid, (i,j))
+                part_value, _ = get_number_value(grid, (i,j))
+                total += part_value
 
     return total 
 
-def solve_part_2(puzzle_input, dimensions):
-    return ""
+def solve_part_2(grid, dimensions):
+    max_i, max_j = dimensions
+    gears = defaultdict(list)
+    for j in range(max_j+1):
+        for i in range(max_i+1):
+            char = grid[(i,j)]
+            if is_digit(char) and (not is_digit(grid[(i-1,j)])):
+                part_value, gear_coords = get_number_value(grid, (i,j))
+                if gear_coords != NO_GEAR:
+                    gears[gear_coords].append(part_value)
+
+    total = 0
+    for gear_pair in gears.values():
+        if len(gear_pair) < 2:
+            continue
+
+        assert len(gear_pair) == 2
+
+        total += gear_pair[0] * gear_pair[1]
+    return total 
 
 if __name__ == "__main__":
     puzzle_input, dimensions = get_puzzle_input()
